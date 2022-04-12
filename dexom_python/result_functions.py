4536d6a9f24a6d3e173c80a7c1da9ba29b0b286c
mstingl <maximilian.ha.stingl@gmail.com>
2022-04-12 10:23:26 +0200

    removed get_binary_sol

--- dexom_python/result_functions.py
+++ dexom_python/result_functions.py
@@ -8,37 +8,32 @@ from sklearn.decomposition import PCA
 import argparse
 
 
-def get_binary_sol(solution, threshold, tolerance):
-    binary = [1 if np.abs(flux) >= (threshold-tolerance) else 0 for flux in solution.fluxes]
-    return binary
+# def legacy_get_obj_value_from_binary(binary, reaction_weights, full_weights=True, model=None):
+#     """
+#     Calculates the objective value of a solution,
+#     as well as the fraction of active RH and inactive RL reactions
+#
+#     !! The result is not always accurate !!
+#     Specific case: if a RH reaction has flux below epsilon but above threshold, it will be regarded as activated,
+#     but without counting for the objective value
+#     """
+#     if full_weights:
+#         wei = list(reaction_weights.values())
+#     else:
+#         wei = [reaction_weights.get(rxn.id, 0) for rxn in model.reactions]
+#     obj_val = sum([wei[i]*binary[i] if wei[i] >= 0 else -wei[i]*(1-binary[i]) for i in range(len(wei))])
+#     max_obj = sum([abs(x) for x in reaction_weights.values()])
+#     pos_wei = sum([1 for x in reaction_weights.values() if x > 0])
+#     pos_true = sum([1 for i in range(len(wei)) if wei[i] > 0 and binary[i] == 1])
+#     neg_wei = sum([1 for x in reaction_weights.values() if x < 0])
+#     neg_true = sum([1 for i in range(len(wei)) if wei[i] < 0 and binary[i] == 0])
+#     print("objective value: %.2f" % obj_val)
+#     print("maximal possible value: %.2f (%.2f%s, %.2f%s)" % (max_obj, 100*pos_true/pos_wei, "% RH",
+#                                                              100*neg_true/neg_wei, "% RL"))
+#     return obj_val
 
 
-def legacy_get_obj_value_from_binary(binary, reaction_weights, full_weights=True, model=None):
-    """
-    Calculates the objective value of a solution,
-    as well as the fraction of active RH and inactive RL reactions
-
-    !! The result is not always accurate !!
-    Specific case: if a RH reaction has flux below epsilon but above threshold, it will be regarded as activated,
-    but without counting for the objective value
-    """
-    if full_weights:
-        wei = list(reaction_weights.values())
-    else:
-        wei = [reaction_weights.get(rxn.id, 0) for rxn in model.reactions]
-    obj_val = sum([wei[i]*binary[i] if wei[i] >= 0 else -wei[i]*(1-binary[i]) for i in range(len(wei))])
-    max_obj = sum([abs(x) for x in reaction_weights.values()])
-    pos_wei = sum([1 for x in reaction_weights.values() if x > 0])
-    pos_true = sum([1 for i in range(len(wei)) if wei[i] > 0 and binary[i] == 1])
-    neg_wei = sum([1 for x in reaction_weights.values() if x < 0])
-    neg_true = sum([1 for i in range(len(wei)) if wei[i] < 0 and binary[i] == 0])
-    print("objective value: %.2f" % obj_val)
-    print("maximal possible value: %.2f (%.2f%s, %.2f%s)" % (max_obj, 100*pos_true/pos_wei, "% RH",
-                                                             100*neg_true/neg_wei, "% RL"))
-    return obj_val
-
-
-def write_solution(solution, threshold, filename="imat_sol.csv"):
+def write_solution(model, solution, threshold, filename="imat_sol.csv"):
     """
     Writes an optimize solution as a txt file. The solution is written in a column format
     Parameters
@@ -47,7 +42,8 @@ def write_solution(solution, threshold, filename="imat_sol.csv"):
     threshold: float
     filename: str
     """
-    solution_binary = get_binary_sol(solution, threshold)
+    tol = model.solver.configuration.tolerances.feasibility
+    solution_binary = (np.abs(solution.fluxes) >= threshold-tol).values.astype(int)
 
     with open(filename, "w+") as file:
         file.write("reaction,fluxes,binary\n")
@@ -55,6 +51,7 @@ def write_solution(solution, threshold, filename="imat_sol.csv"):
             file.write(solution.fluxes.index[i]+","+str(v)+","+str(solution_binary[i])+"\n")
         file.write("objective value: %f\n" % solution.objective_value)
         file.write("solver status: %s" % solution.status)
+    return solution, solution_binary
 
 
 def read_solution(filename, model=None, reaction_weights=None):
@@ -93,47 +90,47 @@ def combine_solutions(sol_path):
     uniquesol.to_csv(sol_path+"/combined_solutions.csv")
     return uniquesol
 
-
-def detect_problems(model, eps, thr, tol):
-    primals_indicators = {}
-    for key, val in model.solver.primal_values.items():
-        if "rh_" in key or "rl_" in key[:3]:
-            primals_indicators[key] = val
-
-    primals_fluxes = {}
-    for key, val in model.solver.primal_values.items():
-        if key not in primals_indicators.keys():
-            if "reverse" not in key:
-                primals_fluxes[key] = val
-            else:
-                newkey = "_".join(key.split("_")[:2])
-                primals_fluxes[newkey] = val
-
-    problems = {}
-    for key, val in primals_indicators.items():
-        k = key.split("_")
-        if k[0] == "rl":
-            flux = primals_fluxes[k[1]] + primals_fluxes[k[1] + "_reverse"]
-            if val > 0.5 and flux < thr: # CHANGED FOR TESTING
-                problems[key] = (flux, val)
-            elif val < 0.5 and flux > thr: # CHANGED FOR TESTING
-                problems[key] = (flux, val)
-        elif k[-1] == "pos":
-            flux = primals_fluxes[k[1]]
-            if val > 0.5 and flux < eps-1000*tol:
-                problems[key] = (flux, val)
-            elif val < 0.5 and flux >= eps-1000*tol:
-                problems[key] = (flux, val)
-        elif k[-1] == "neg":
-            flux = primals_fluxes[k[1] + "_reverse"]
-            if val > 0.5 and flux < eps-1000*tol:
-                problems[key] = (flux, val)
-            elif val < 0.5 and flux >= eps-1000*tol:
-                problems[key] = (flux, val)
-        else:
-            print("problem with", key)
-
-    return problems
+#
+# def detect_problems(model, eps, thr, tol):
+#     primals_indicators = {}
+#     for key, val in model.solver.primal_values.items():
+#         if "rh_"[:3] in key or "rl_" in key[:3]:
+#             primals_indicators[key] = val
+#
+#     primals_fluxes = {}
+#     for key, val in model.solver.primal_values.items():
+#         if key not in primals_indicators.keys():
+#             if "reverse" not in key:
+#                 primals_fluxes[key] = val
+#             else:
+#                 newkey = "_".join(key.split("_")[:2])
+#                 primals_fluxes[newkey] = val
+#
+#     problems = {}
+#     for key, val in primals_indicators.items():
+#         k = key.split("_")
+#         if k[0] == "rl":
+#             flux = primals_fluxes[k[1]] + primals_fluxes[k[1] + "_reverse"]
+#             if val > 0.5 and flux < (thr-tol):  # uses new variable implementation
+#                 problems[key] = (flux, val)
+#             elif val < 0.5 and flux >= (thr-tol):  # uses new variable implementation
+#                 problems[key] = (flux, val)
+#         elif k[-1] == "pos":
+#             flux = primals_fluxes[k[1]]
+#             if val > 0.5 and flux < (eps-1000*tol):
+#                 problems[key] = (flux, val)
+#             elif val < 0.5 and flux >= (eps-1000*tol):
+#                 problems[key] = (flux, val)
+#         elif k[-1] == "neg":
+#             flux = primals_fluxes[k[1] + "_reverse"]
+#             if val > 0.5 and flux < (eps-1000*tol):
+#                 problems[key] = (flux, val)
+#             elif val < 0.5 and flux >= (eps-1000*tol):
+#                 problems[key] = (flux, val)
+#         else:
+#             print("problem with", key)
+#
+#     return problems
 
 
 def plot_pca(solution_path, rxn_enum_solutions=None, save_name=""):
